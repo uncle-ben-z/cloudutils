dc09b23dd7b6a482b36e6a00b43a3b76bd3c82e2
Christian Benz <christian.benz@uni-weimar.de>
2021-09-24 10:05:19 +0200

    Extend for meta-clusters (exposed rebars)

--- clustering.py
+++ clustering.py
@@ -1,6 +1,6 @@
 import numpy as np
-from tqdm import tqdm
 import pandas as pd
+from tqdm import tqdm
 from pyntcloud import PyntCloud
 from sklearn.cluster import DBSCAN
 
@@ -12,11 +12,12 @@ def cluster_point_cloud(cloud_path, result_path, eps=0.005, min_samples=3):
     xyz = ply.xyz
     defects = np.array(ply.points['defect'])
     clusters = np.int32(np.copy(defects) * 0)
+    meta_clusters = np.int32(np.copy(defects) * 0)
 
     # loop over all defect classes
-    for d in tqdm([1,2,3,4,6]):
+    for d in tqdm(np.unique(defects)):
         # filter for current defect class
-        if d == 4:
+        if d == 5:
             # account for exposed rebars
             idxs = np.where((defects == 4) | (defects == 5), True, False)
         else:
@@ -26,23 +27,28 @@ def cluster_point_cloud(cloud_path, result_path, eps=0.005, min_samples=3):
         # perform clustering
         clustering = DBSCAN(eps=eps, min_samples=min_samples).fit(X)
 
+        # special case exposed rebar
+        if d == 5:
+            meta_clusters[idxs] = clustering.labels_ + 1
+            continue
+
         # assign unique ids for clusters
         clusters[idxs] = clustering.labels_ + np.max(clusters) + 1
 
         # assign 0 to invalid clusters
-        tmp = clusters[idxs] # tmp variable is needed
+        tmp = clusters[idxs]  # tmp variable is needed
         tmp[(clustering.labels_ == -1)] = 0
         clusters[idxs] = tmp
 
     # set property and save cloud
     ply.points['cluster'] = pd.Series(clusters)
+    ply.points['meta_cluster'] = pd.Series(meta_clusters)
     ply.to_file(result_path)
 
     return ply
 
 
-
-
 if __name__ == "__main__":
-    cloud_path = "/home/******/repos/defect-demonstration/static/uploads/mtb/points_colorized.ply" #"/home/******/repos/defect-demonstration/static/uploads/mtb/ausschnitt.ply"#/home/******/repos/defect-demonstration/static/uploads/2021_07_20__15_19_17/new_result_cloud.ply"
-    cluster_point_cloud(cloud_path=cloud_path, result_path="/home/******/repos/defect-demonstration/static/uploads/mtb/points_colorized_clustered2.ply")#"/home/******/repos/defect-demonstration/static/uploads/mtb/ausschnitt_cluster.ply")
+    cloud_path = "/home/******/repos/defect-demonstration/static/uploads/mtb/ausschnitt_colorized.ply"
+    cluster_point_cloud(cloud_path=cloud_path,
+                        result_path="/home/******/repos/defect-demonstration/static/uploads/mtb/ausschnitt_clustered.ply")
