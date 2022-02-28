2fb54a929c8c2fe5b886bd3f13c2fc8a6858b034
Christian Benz <christian.benz@uni-weimar.de>
2022-02-28 14:26:13 +0100

    Add renderview.py

--- renderview.py
+++ renderview.py
@@ -2,10 +2,12 @@ import os
 import cv2
 import Metashape
 import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.use("TkAgg")
 
-
-def render_view(path, xml_path, ply_path):
-    """ Uses agisoft to render the absolute depth to the dense point cloud. """
+def render_views(model_path, cameras_path, path, is_cloud=False):
+    """ Uses agisoft to render views. """
     doc = Metashape.Document()
     chunk = doc.addChunk()
 
@@ -15,28 +17,41 @@ def render_view(path, xml_path, ply_path):
     images = os.listdir(path)
     images = [os.path.join(path, elem) for elem in images]
 
-    if len(images) == len(os.listdir(outpath)):
-        print("depth already computed")
+    if False and len(images) == len(os.listdir(outpath)):
+        print("Views already rendered...")
         return
 
-    chunk.addPhotos(images)  # photos need to be added
-    chunk.importCameras(xml_path)
-    chunk.importPoints(ply_path)
+    chunk.addPhotos(images)  # photos need to be added # TODO: when rendering new images, no images so far known
+    chunk.importCameras(cameras_path)
+
+    if is_cloud:
+        chunk.importPoints(model_path)
+    else:
+        chunk.importModel(model_path)
 
     for j, camera in enumerate(chunk.cameras):
         print(camera.label)
         if not camera.enabled or camera.transform is None:
             continue
 
-        if os.path.exists(os.path.join(outpath, camera.label) + ".png"):
+        if False and os.path.exists(os.path.join(outpath, camera.label) + ".png"):
             continue
 
         # render image
-        img = chunk.dense_cloud.renderImage(camera.transform, camera.sensor.calibration, point_size=1)
+        if is_cloud:
+            img = chunk.dense_cloud.renderImage(camera.transform, camera.sensor.calibration, point_size=1)
+        else:
+            img = chunk.model.renderImage(camera.transform, camera.sensor.calibration)
+        img.save(os.path.join(outpath, camera.label + ".png"))
         byts = img.tostring()
 
         # convert to numpy
         img_np = np.copy(np.frombuffer(byts, dtype=np.uint8))
         img_np = img_np.reshape(img.height, img.width, 4)
 
+        #plt.imshow(img_np[..., :3])
+        #plt.show()
+
+        img_np = cv2.cvtColor(img_np, cv2.COLOR_RGBA2BGR)
         cv2.imwrite(os.path.join(outpath, camera.label + ".png"), img_np)
+
